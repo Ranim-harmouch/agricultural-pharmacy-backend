18c8b31bc3e445ad88212d669bc30b1be60d4d6c
ranim <ranim17har@gmail.com>
2025-05-25 13:13:23 +0300

    .

--- server.js
+++ server.js
@@ -20,29 +20,17 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 // app.use(cors()); // If needed
+
 app.use(cors({
   origin: [ 'http://localhost:3000' ,
   'https://boisterous-zuccutto-c1c010.netlify.app'], // React app’s address
-  credentials: true,                  // allow cookies to be sent
-  optionsSuccessStatus: 204
+  credentials: true
 }));
+
 
 app.use(express.json()); // To parse JSON bodies
 app.use(cookieParser());
-app.use(cors(corsOptions));
 
-// Middleware to add Access-Control-Allow-Private-Network header for preflight requests
-app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', corsOptions.origin.includes(req.headers.origin) ? req.headers.origin : '');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  
-  // Add this to opt-in for private network access
-  res.header('Access-Control-Allow-Private-Network', 'true');
-  
-  res.sendStatus(204);
-});
 
 app.use('/api/products', productRoutes);
 
